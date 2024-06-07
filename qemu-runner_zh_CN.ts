aad13fff886fff0e92259d8ec0310f4625fd20ff
gfdgd_xi <3025613752@qq.com>
2024-06-07 18:36:55 +0800

    使用cmake构建

--- qemu-runner_zh_CN.ts
+++ qemu-runner_zh_CN.ts
@@ -1,3 +1,171 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="ui_mainwindow.h" line="328"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="25"/>
+        <location filename="ui_mainwindow.h" line="342"/>
+        <source>简易</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="33"/>
+        <location filename="ui_mainwindow.h" line="329"/>
+        <source>Legecy（传统启动）</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <location filename="ui_mainwindow.h" line="330"/>
+        <source>UEFI启动</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <location filename="ui_mainwindow.h" line="331"/>
+        <source>kvm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="71"/>
+        <location filename="ui_mainwindow.h" line="333"/>
+        <source>启动方式：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="78"/>
+        <location filename="ui_mainwindow.h" line="334"/>
+        <source>硬件加速：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <location filename="ui_mainwindow.h" line="335"/>
+        <source>/usr/bin/qemu-system-x86_64</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="113"/>
+        <location filename="ui_mainwindow.h" line="337"/>
+        <source>Qemu选择：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="120"/>
+        <location filename="ui_mainwindow.h" line="338"/>
+        <source>ISO文件：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="127"/>
+        <location filename="ui_mainwindow.h" line="339"/>
+        <source>磁盘：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="134"/>
+        <location filename="mainwindow.ui" line="141"/>
+        <location filename="ui_mainwindow.h" line="340"/>
+        <location filename="ui_mainwindow.h" line="341"/>
+        <source>浏览</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="149"/>
+        <location filename="ui_mainwindow.h" line="357"/>
+        <source>高级</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="155"/>
+        <location filename="ui_mainwindow.h" line="343"/>
+        <source>CPU核心数：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="162"/>
+        <location filename="ui_mainwindow.h" line="344"/>
+        <source>VNC 端口：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="171"/>
+        <location filename="ui_mainwindow.h" line="345"/>
+        <source>全屏显示</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="181"/>
+        <location filename="ui_mainwindow.h" line="346"/>
+        <source>virt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="190"/>
+        <location filename="ui_mainwindow.h" line="348"/>
+        <source>host</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="198"/>
+        <location filename="ui_mainwindow.h" line="350"/>
+        <source>引导顺序：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="205"/>
+        <location filename="ui_mainwindow.h" line="351"/>
+        <source>CPU线程数：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="225"/>
+        <location filename="ui_mainwindow.h" line="352"/>
+        <source>机器：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="235"/>
+        <location filename="ui_mainwindow.h" line="353"/>
+        <source>CPU：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="242"/>
+        <location filename="ui_mainwindow.h" line="354"/>
+        <source>显示：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="249"/>
+        <location filename="ui_mainwindow.h" line="355"/>
+        <source>CPU数量：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="256"/>
+        <location filename="ui_mainwindow.h" line="356"/>
+        <source>内存大小：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="297"/>
+        <location filename="ui_mainwindow.h" line="358"/>
+        <source>启用高级选项</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="304"/>
+        <location filename="ui_mainwindow.h" line="359"/>
+        <source>运行 Qemu</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
